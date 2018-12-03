1b69cbc13bc83275aed084d6111899f11462f2ab
iolumbaram <hazel77744@gmail.com>
2018-12-04 05:16:06 +0800

    webpack, babel

---
+++ my-app/webpack.config.js
@@ -0,0 +1,22 @@
+var path    = require('path');
+var hwp     = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: path.join(__dirname, '/src/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist')
+    },
+    module:{
+        rules:[{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins:[
+        new hwp({template:path.join(__dirname, '/src/index.html')}),
+        new webpack.ProvidePlugin({ grapesjs: "grapesjs" })
+    ]
+}
